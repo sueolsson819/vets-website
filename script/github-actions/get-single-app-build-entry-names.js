95d3c2f1f0b207a88631fd928efcf0a5fb78bdcf
Chris Alexander <chris.alexander@adhocteam.us>
2021-10-19 17:51:04 -0600

    Refactor and include allowList

--- script/github-actions/get-single-app-build-entry-names.js
+++ script/github-actions/get-single-app-build-entry-names.js
@@ -3,6 +3,10 @@ const find = require('find');
 const path = require('path');
 const core = require('@actions/core');
 
+const config = require('../../config/single-app-build.json');
+
+const changedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
+
 /**
  * Takes a relative path and returns the entryName of
  * the app that the given path belongs to
@@ -24,18 +28,49 @@ const getEntryName = filePath => {
   return manifestFile.entryName;
 };
 
-const changedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
-let isFullBuild = false;
-const entryNames = [];
-
-changedFiles.forEach(file => {
+/**
+ * If the provided file is part of an app, and that app is in the allow list,
+ * returns the app entry name. Otherwise returns null
+ *
+ * @param {string} file
+ * @param {string[]} allowList
+ * @returns {string|null}
+ */
+const getAllowedEntryName = (file, allowList) => {
   if (file.startsWith('src/applications')) {
-    entryNames.push(getEntryName(file));
-  } else {
-    isFullBuild = true; // All non-app changes require a full build
+    const entryName = getEntryName(file);
+    if (allowList.includes(entryName)) {
+      return entryName;
+    }
   }
-});
 
-// Create a string suitable for the --entry arg, with no duplicates
-const entryString = [...new Set(entryNames)].join(',');
-core.exportVariable('APP_ENTRY_NAMES', isFullBuild ? '' : entryString);
+  return null;
+};
+
+/**
+ * Given a list of files and an app allow list, checks if a single app build
+ * is possible. If so, returns a comma-separated list of app entry names.
+ * If not, returns an empty string
+ *
+ * @param {string[]} files
+ * @param {string[]} allowList
+ * @returns {string}
+ */
+const makeEntryString = (files, allowList) => {
+  const allowedEntries = [];
+
+  for (const file of files) {
+    const entryName = getAllowedEntryName(file, allowList);
+    if (entryName) {
+      allowedEntries.push(entryName);
+    } else {
+      return ''; // A full build is required, no need to continue
+    }
+  }
+
+  // Remove dupes and stringify for use by the --entry argument
+  return [...new Set(allowedEntries)].join(',');
+};
+
+const entryString = makeEntryString(changedFiles, config.allow);
+core.exportVariable('APP_ENTRY_NAMES', entryString);
