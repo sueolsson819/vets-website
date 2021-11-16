bfed83363b21aff9c2c23f3e8dc31d0a99d3b526
BJ McCotter <bj.mccotter@adhocteam.us>
2021-11-16 16:14:59 -0600

    added first form

---
+++ src/applications/bj-test-form/config/form.js
@@ -0,0 +1,78 @@
+// import fullSchema from 'vets-json-schema/dist/22-BJTEST-schema.json';
+
+import manifest from '../manifest.json';
+
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  submitUrl: '/v0/api',
+  trackingPrefix: 'new-form-bj-test-form',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: '22-BJTEST',
+  saveInProgress: {
+    // messages: {
+    //   inProgress: 'Your new form benefits application (22-BJTEST) is in progress.',
+    //   expired: 'Your saved new form benefits application (22-BJTEST) has expired. If you want to apply for new form benefits, please start a new application.',
+    //   saved: 'Your new form benefits application has been saved.',
+    // },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for new form benefits.',
+    noAuth:
+      'Please sign in again to continue your application for new form benefits.',
+  },
+  title: 'bj-test-form',
+  defaultDefinitions: {},
+  chapters: {
+    chapter1: {
+      title: 'Chapter 1',
+      pages: {
+        page1: {
+          path: 'first-page',
+          title: 'First Page',
+          uiSchema: {
+            myField: {
+              'ui:title': 'My field Label',
+              'ui:widget': 'radio',
+              'ui:options': {
+                widgetProps: {
+                  'First option': { 'data-info': 'first_1' },
+                  'Second option': { 'data-info': 'second_2' },
+                },
+                // Only added to the radio when it is selected
+                // a11y requirement: aria-describedby ID's *must* exist on the page;
+                // and we conditionally add content based on the selection
+                selectedProps: {
+                  'First option': { 'aria-describedby': 'some_id_1' },
+                  'Second option': { 'aria-describedby': 'some_id_2' },
+                },
+              },
+            },
+          },
+          schema: {
+            type: 'object',
+            required: ['myField'],
+            properties: {
+              myField: {
+                type: 'string',
+                enum: ['First option', 'Second option'],
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
