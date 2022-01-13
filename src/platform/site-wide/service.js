9ed227f8c162fd125d294bbcedb7039d6685661e
Brooks Johnson <brooks.johnson@pm.me>
2022-01-12 16:52:25 -0800

    Close event when clicked

--- src/platform/site-wide/service.js
+++ src/platform/site-wide/service.js
@@ -1,2 +1,8 @@
 // eslint-disable-next-line no-console
 console.log('Hello from service worker');
+
+self.addEventListener('notificationclick', event => {
+  // eslint-disable-next-line no-console
+  console.log('Notification clicked, closing notification');
+  event.notification.close();
+});
