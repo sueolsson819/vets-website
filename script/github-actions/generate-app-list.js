17639630b3986290b91e3c804b82de32b238000d
Holden Hinkle <holden@holdenhinkle.com>
2022-01-19 17:02:25 -0500

    try an array of arrays instead of object

--- script/github-actions/generate-app-list.js
+++ script/github-actions/generate-app-list.js
@@ -3,13 +3,11 @@ const core = require('@actions/core');
 const { getAppManifests } = require('../../config/manifest-helpers');
 
 function exportAppList() {
-  const applicationList = getAppManifests().map(app => {
-    return {
-      name: app.appName,
-      slug: app.entryName,
-    };
-  });
-  core.exportVariable('APPLICATION_LIST', JSON.stringify(applicationList));
+  const applicationList = [];
+  getAppManifests().forEach(app =>
+    applicationList.push([app.appName, app.entryName]),
+  );
+  core.exportVariable('APPLICATION_LIST', applicationList);
 }
 
 exportAppList();
