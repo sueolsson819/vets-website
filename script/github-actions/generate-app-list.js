9fe5f55d891e40426a6e3ba177a8fa56abcb8d0c
Holden Hinkle <holden@holdenhinkle.com>
2022-01-19 17:13:28 -0500

    cleanup

--- script/github-actions/generate-app-list.js
+++ script/github-actions/generate-app-list.js
@@ -4,9 +4,11 @@ const { getAppManifests } = require('../../config/manifest-helpers');
 
 function exportAppList() {
   const applicationList = [];
+
   getAppManifests().forEach(app =>
     applicationList.push([app.appName, app.entryName]),
   );
+
   core.exportVariable('APPLICATION_LIST', applicationList);
 }
 
