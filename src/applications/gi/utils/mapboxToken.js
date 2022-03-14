1a41216a1add8af43bf8ddb2c9610ea2e0864b2c
Ray Messina <rmessina10@gmail.com>
2022-03-14 18:45:15 -0500

    Add imports of process.env vars to mapboxToken.js files utilizing dotenv-webpack

--- src/applications/gi/utils/mapboxToken.js
+++ src/applications/gi/utils/mapboxToken.js
@@ -1,2 +1,5 @@
-export const mapboxToken =
-  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2tvbHI5aDQ2MDAxaTJvcW5xYThremM1MyJ9.pEZjlJcYPfCNwoUEnMnagg';
+import environment from 'platform/utilities/environment';
+
+export const mapboxToken = environment.isProduction()
+  ? process.env.REACT_APP_VALocatorProd
+  : process.env.REACT_APP_VALocatorDev;
