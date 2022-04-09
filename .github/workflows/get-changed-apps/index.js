12775043384e802a225cc47e7e5432da285d1777
Theo Bentum <theo@adhocteam.us>
2022-04-08 20:16:39 -0700

    Change name

--- .github/workflows/get-changed-apps/index.js
+++ .github/workflows/get-changed-apps/index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 
 try {
-  console.log(`::set-output name=app_entries::vaos`);
-  core.setOutput('app_entries', 'check-in');
+  // console.log(`::set-output name=app_entries::vaos`);
+  core.setOutput('entry_names', 'check-in');
 } catch (error) {
   core.setFailed(error.message);
 }
