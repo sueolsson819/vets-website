78efbdf72c704e828415f60611453a1b14df981e
Darius Dzien <dariusz.dzien@adhocteam.us>
2022-04-15 10:59:36 -0500

    Fix test path

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -9,7 +9,7 @@ const divider = Math.ceil(tests.length / numContainers);
 const appUrl = process.env.APP_URLS.split(',')[0];
 
 const batch = tests
-  .map(test => test.replace('/home/runner/work', '/__w'))
+  .map(test => test.replace('/home/runner/work/vets-website/vets-website/', ''))
   .slice(step * divider, (step + 1) * divider)
   .join(',');
 
