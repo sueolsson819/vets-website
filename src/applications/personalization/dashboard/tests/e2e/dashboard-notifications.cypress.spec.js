2b028a60a131ce607b05efab957851185bb150e7
Tze-chiu Lei <tze@adhocteam.us>
2022-06-03 11:54:13 -0700

    Move logins & optimize waits in My VA Dashboard-Notifications E2E-spec

--- src/applications/personalization/dashboard/tests/e2e/dashboard-notifications.cypress.spec.js
+++ src/applications/personalization/dashboard/tests/e2e/dashboard-notifications.cypress.spec.js
@@ -20,7 +20,7 @@ import { mockLocalStorage } from '~/applications/personalization/dashboard/tests
 
 describe(
   'The My VA Dashboard - Notifications',
-  { includeShadowDom: true },
+  { includeShadowDom: true, defaultCommandTimeout: 12000 },
   () => {
     describe('when the feature is hidden', () => {
       beforeEach(() => {
@@ -37,18 +37,16 @@ describe(
         mockLocalStorage();
         cy.login(mockUser);
       });
+
       it('the notifications does not show up - C13978', () => {
         cy.visit('my-va/');
-        cy.waitFor200s(['@featuresA', '@nameA', '@serviceA']).then(() => {
-          cy.findByTestId('dashboard-notifications').should('not.exist');
-        });
-
-        // make the a11y check
+        cy.wait('@featuresA');
+        cy.findByTestId('dashboard-notifications').should('not.exist');
         cy.injectAxeThenAxeCheck();
       });
     });
+
     describe('when the feature is not hidden', () => {
-      Cypress.config({ defaultCommandTimeout: 12000 });
       beforeEach(() => {
         cy.intercept('GET', '/v0/feature_toggles*', {
           data: {
@@ -66,7 +64,9 @@ describe(
         );
         cy.intercept('GET', '/v0/profile/full_name', fullName).as('nameB');
         mockLocalStorage();
+        cy.login(mockUser);
       });
+
       /* eslint-disable va/axe-check-required */
       // Same display-state as previous test already AXE-checked.
       it('and they have no notifications - C13979', () => {
@@ -75,81 +75,70 @@ describe(
           '/v0/onsite_notifications',
           notificationsSuccessEmpty(),
         ).as('notifications1');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications1').then(() => {
-          cy.findByTestId('dashboard-notifications').should('not.exist');
-        });
+        cy.wait('@notifications1');
+        cy.findByTestId('dashboard-notifications').should('not.exist');
       });
       /* eslint-enable va/axe-check-required */
+
       it('and they have a notification - C13025', () => {
         cy.intercept(
           'GET',
           '/v0/onsite_notifications',
           notificationSuccessNotDismissed(),
         ).as('notifications2');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications2').then(() => {
-          cy.findByTestId('dashboard-notifications').should('exist');
-          cy.findAllByTestId('dashboard-notification-alert').should(
-            'have.length',
-            1,
-          );
-          // make the a11y check
-          cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
-        });
+        cy.wait('@notifications2');
+        cy.findByTestId('dashboard-notifications').should('exist');
+        cy.findAllByTestId('dashboard-notification-alert').should(
+          'have.length',
+          1,
+        );
+        // make the a11y check
+        cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
       });
+
       it('and they have multiple notifications - C16720', () => {
         cy.intercept(
           'GET',
           '/v0/onsite_notifications',
           multipleNotificationSuccess(),
         ).as('notifications3');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications3').then(() => {
-          cy.findByTestId('dashboard-notifications').should('exist');
-          cy.findAllByTestId('dashboard-notification-alert').should(
-            'have.length',
-            2,
-          );
-          // make the a11y check
-          cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
-        });
+        cy.wait('@notifications3');
+        cy.findByTestId('dashboard-notifications').should('exist');
+        cy.findAllByTestId('dashboard-notification-alert').should(
+          'have.length',
+          2,
+        );
+        cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
       });
+
       it('and they have dismissed notifications - C16721', () => {
         cy.intercept(
           'GET',
           '/v0/onsite_notifications',
           notificationSuccessDismissed(),
         ).as('notifications4');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications4').then(() => {
-          cy.findByTestId('dashboard-notifications').should('not.exist');
-
-          // make the a11y check
-          cy.injectAxeThenAxeCheck('#react-root');
-        });
+        cy.wait('@notifications4');
+        cy.findByTestId('dashboard-notifications').should('not.exist');
+        cy.injectAxeThenAxeCheck('#react-root');
       });
+
       it('and they have a notification error - C16722', () => {
         cy.intercept(
           'GET',
           '/v0/onsite_notifications',
           notificationsError(),
         ).as('notifications5');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications5').then(interception => {
-          expect(interception.response.body.errors).to.have.length.gte(1);
-          cy.findByTestId('dashboard-notifications').should('exist');
-          cy.findByTestId('dashboard-notifications-error').should('exist');
-
-          // make the a11y check
-          cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
-        });
+        cy.wait('@notifications5');
+        cy.findByTestId('dashboard-notifications').should('exist');
+        cy.findByTestId('dashboard-notifications-error').should('exist');
+        cy.injectAxeThenAxeCheck('[data-testid="dashboard-notifications"]');
       });
+
       /* eslint-disable va/axe-check-required */
       // Same display-state as previous test already AXE-checked.
       it('and they dismiss a notification - C16723', () => {
@@ -167,22 +156,18 @@ describe(
             delay: 100,
           },
         ).as('patch');
-        cy.login(mockUser);
         cy.visit('my-va/');
-        cy.waitFor200s('@notifications6').then(() => {
-          cy.findByTestId('dashboard-notifications').should('exist');
-          cy.findAllByTestId('dashboard-notification-alert').should(
-            'have.length',
-            1,
-          );
-          cy.get('va-alert')
-            .shadow()
-            .find('button.va-alert-close')
-            .click({ waitForAnimations: true });
-          cy.waitFor200s('@patch').then(() => {
-            cy.findByTestId('dashboard-notifications').should('not.exist');
-          });
-        });
+        cy.wait('@notifications6');
+        cy.findByTestId('dashboard-notifications').should('exist');
+        cy.findAllByTestId('dashboard-notification-alert').should(
+          'have.length',
+          1,
+        );
+        cy.get('va-alert')
+          .find('button.va-alert-close')
+          .click({ waitForAnimations: true });
+        cy.wait('@patch');
+        cy.findByTestId('dashboard-notifications').should('not.exist');
       });
       /* eslint-enable va/axe-check-required */
     });
